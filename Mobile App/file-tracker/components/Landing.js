428c8756883898486ab197670540ca9e35bc6bbb
hansinahuja <hansinahuja@gmail.com>
2021-02-21 15:54:09 +0530

    Add textured background

--- Mobile App/file-tracker/components/Landing.js
+++ Mobile App/file-tracker/components/Landing.js
@@ -6,7 +6,12 @@ import {
   Paragraph,
   Caption,
 } from "react-native-paper";
-import { View, RefreshControl, ScrollView } from "react-native";
+import {
+  View,
+  RefreshControl,
+  ScrollView,
+  ImageBackground,
+} from "react-native";
 // import ScrollView from "expo-faded-scrollview";
 import { AntDesign } from "@expo/vector-icons";
 
@@ -15,147 +20,156 @@ const Landing = () => {
   const [files, setFiles] = useState([]);
 
   return (
-    <View style={{ flex: 1, backgroundColor: "black" }}>
-      <View
-        style={{
-          height: "30%",
-          alignItems: "center",
-          justifyContent: "center",
+    <>
+      <ImageBackground
+        style={{ flex: 1, resizeMode: "cover" }}
+        source={{
+          uri: "https://wallpapercave.com/wp/Dp3Xq6o.jpg",
         }}
+        resizeMode={"cover"} // cover or contain its upto you view look
       >
-        <Button
-          mode="outlined"
-          style={{
-            justifyContent: "center",
-            borderColor: "white",
-            borderWidth: 0.5,
-            marginTop: "10%",
-            width: "65%",
-          }}
-          color="white"
-          onPress={() => {}}
-        >
-          <AntDesign name="addfile" size={14} color="white" />
-          {"   "}Create new file
-        </Button>
-        <Button
-          mode="outlined"
-          style={{
-            justifyContent: "center",
-            borderColor: "white",
-            borderWidth: 0.5,
-            marginTop: "5%",
-            width: "65%",
-          }}
-          color="white"
-          onPress={() => {}}
-        >
-          <AntDesign name="search1" size={14} color="white" />
-          {"   "}Track existing file
-        </Button>
-      </View>
-      <View
-        style={{
-          backgroundColor: "white",
-          borderTopLeftRadius: 50,
-          borderTopRightRadius: 50,
-          overflow: "hidden",
-          flex: 1,
-          alignItems: "center",
-          paddingTop: "0%",
-        }}
-      >
-        <View style={{ flex: 1, width: "100%" }}>
-          <ScrollView
+        <View style={{ flex: 1, backgroundColor: "transparent" }}>
+          <View
             style={{
-              width: "100%",
-            }}
-            contentContainerStyle={{
+              height: "30%",
               alignItems: "center",
-              paddingBottom: "15%",
+              justifyContent: "center",
             }}
-            refreshControl={
-              <RefreshControl
-                refreshing={refreshing}
-                onRefresh={() => {
-                  // dummy logic
-                  var new_files = [
-                    {
-                      name: "Budget Approval",
-                      status: "Pending Dean's approval",
-                      trackingID: String(
-                        Math.floor(Math.random() * (1000000 - 100000) + 100000)
-                      ),
-                    },
-                  ].concat(files);
-                  setFiles(new_files);
-                }}
-              />
-            }
           >
-            <View style={{ marginTop: "4%" }}>
-              <Subheading style={{ fontSize: 18 }}>
-                Your tracking history
-              </Subheading>
-            </View>
-            {files.map((file, idx) => {
-              return (
-                <View
-                  style={{
-                    width: "85%",
-                    height: 100,
-                    borderColor: "black",
-                    borderWidth: 1,
-                    borderRadius: 10,
-                    marginTop: idx === 0 ? "3%" : "4%",
-                    overflow: "hidden",
-                  }}
-                  key={file.trackingID}
-                >
-                  <TouchableRipple
-                    onPress={() => {}}
-                    rippleColor="rgba(0, 0, 0, .15)"
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      justifyContent: "center",
+            <Button
+              mode="outlined"
+              style={{
+                justifyContent: "center",
+                borderColor: "white",
+                borderWidth: 0.5,
+                marginTop: "10%",
+                width: "65%",
+              }}
+              color="white"
+              onPress={() => {}}
+            >
+              <AntDesign name="addfile" size={14} color="white" />
+              {"   "}Create new file
+            </Button>
+            <Button
+              mode="outlined"
+              style={{
+                justifyContent: "center",
+                borderColor: "white",
+                borderWidth: 0.5,
+                marginTop: "5%",
+                width: "65%",
+              }}
+              color="white"
+              onPress={() => {}}
+            >
+              <AntDesign name="search1" size={14} color="white" />
+              {"   "}Track existing file
+            </Button>
+          </View>
+          <View
+            style={{
+              backgroundColor: "white",
+              borderTopLeftRadius: 25,
+              borderTopRightRadius: 25,
+              overflow: "hidden",
+              flex: 1,
+              alignItems: "center",
+              paddingTop: "0%",
+            }}
+          >
+            <View style={{ flex: 1, width: "100%" }}>
+              <ScrollView
+                style={{
+                  width: "100%",
+                }}
+                contentContainerStyle={{
+                  alignItems: "center",
+                  paddingBottom: "15%",
+                }}
+                refreshControl={
+                  <RefreshControl
+                    refreshing={refreshing}
+                    onRefresh={() => {
+                      // dummy logic
+                      var new_files = [
+                        {
+                          name: "Budget Approval",
+                          status: "Pending Dean's approval",
+                          trackingID: String(
+                            Math.floor(
+                              Math.random() * (1000000 - 100000) + 100000
+                            )
+                          ),
+                        },
+                      ].concat(files);
+                      setFiles(new_files);
                     }}
-                  >
-                    <View style={{ flexDirection: "row" }}>
-                      <View
-                        style={{ paddingVertical: "2%", paddingLeft: "5%" }}
-                      >
-                        <Subheading style={{ fontWeight: "bold" }}>
-                          {file.name}
-                        </Subheading>
-                        <Paragraph style={{ fontStyle: "italic" }}>
-                          {file.status}
-                        </Paragraph>
-                        <Caption>Tracking ID: {file.trackingID}</Caption>
-                        {/* <Caption>
-                    <Entypo name="calendar" size={14} color="rgba(0,0,0,0.54)" />
-                    {"  "}21st February, 2021
-                  </Caption> */}
-                      </View>
-                      <View
+                  />
+                }
+              >
+                <View style={{ marginTop: "4%" }}>
+                  <Subheading style={{ fontSize: 18 }}>
+                    Your tracking history
+                  </Subheading>
+                </View>
+
+                {files.map((file, idx) => {
+                  return (
+                    <View
+                      style={{
+                        width: "85%",
+                        height: 100,
+                        borderColor: "black",
+                        borderWidth: 1,
+                        borderRadius: 10,
+                        marginTop: idx === 0 ? "3%" : "4%",
+                        overflow: "hidden",
+                      }}
+                      key={file.trackingID}
+                    >
+                      <TouchableRipple
+                        onPress={() => {}}
+                        rippleColor="rgba(0, 0, 0, .15)"
                         style={{
-                          flex: 1,
-                          alignItems: "flex-end",
+                          width: "100%",
+                          height: "100%",
                           justifyContent: "center",
-                          paddingRight: "5%",
                         }}
                       >
-                        <AntDesign name="right" size={16} color="black" />
-                      </View>
+                        <View style={{ flexDirection: "row" }}>
+                          <View
+                            style={{ paddingVertical: "2%", paddingLeft: "5%" }}
+                          >
+                            <Subheading style={{ fontWeight: "bold" }}>
+                              {file.name}
+                            </Subheading>
+                            <Paragraph style={{ fontStyle: "italic" }}>
+                              {file.status}
+                            </Paragraph>
+                            <Caption>Tracking ID: {file.trackingID}</Caption>
+                          </View>
+                          <View
+                            style={{
+                              flex: 1,
+                              alignItems: "flex-end",
+                              justifyContent: "center",
+                              paddingRight: "5%",
+                            }}
+                          >
+                            <AntDesign name="right" size={16} color="black" />
+                          </View>
+                        </View>
+                      </TouchableRipple>
                     </View>
-                  </TouchableRipple>
-                </View>
-              );
-            })}
-          </ScrollView>
+                  );
+                })}
+              </ScrollView>
+            </View>
+          </View>
         </View>
-      </View>
-    </View>
+      </ImageBackground>
+    </>
   );
 };
 
