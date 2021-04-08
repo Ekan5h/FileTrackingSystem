cf06391f24a58b83c19507471b5df83bab3703b9
Sakshay Mahna <sakshum19@gmail.com>
2021-04-08 19:43:19 +0530

    Some error with setName

--- Web App/main.py
+++ Web App/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template , request , jsonify
+from flask import Flask, session, render_template , request , jsonify, redirect
+import requests
 
 app = Flask(__name__)
+app.secret_key = "not a secret key"
 
 @app.route('/')
 def root():
@@ -10,6 +12,48 @@ def root():
 def home():
 	return render_template('index.jinja2')
 
+@app.route('/login')
+def login():
+	return render_template('login.jinja2')
+
+@app.route('/otp', methods=['POST'])
+def otp():
+	form_data = {'email' : request.form["email"]}
+	response = requests.post(url = "http://10.10.9.72:5000/generateOTP", data = form_data).json()
+
+	if response['error'] == False:
+		session['email'] = form_data["email"]
+		return render_template('otp.jinja2', context = {"email" : form_data["email"]})
+	else:
+		return response['msg']
+
+@app.route('/redirect', methods=['POST'])
+def redirect():
+	form_data = {"email" : session["email"], "otp" : request.form["otp"], "login": True}
+	response = requests.post(url = "http://10.10.9.72:5000/verifyOTP", data = form_data).json()
+
+	if response["match"]:
+		if response['name'] != '' and response['office'] != '':
+			context = {"email" : session["email"], "name" : response['name'], "offices" : response["offices"]}
+			return render_template('dashboard.jinja2', context = context)
+		else:
+			return render_template('set_name.jinja2')
+	else:
+		return response["msg"]
+
+@app.route('/set_name', methods=["POST"])
+def set_name():
+	form_data = {"name" : request.form["name"]}
+	response = requests.post(url = "http://10.10.9.72:5000/setName", data = form_data).json()
+
+	if response["error"] == False:
+		return redirect("/dashboard")
+	else:
+		return response["msg"]
+
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.jinja2')
 
 if __name__ == '__main__':
 	app.run(debug = True)
