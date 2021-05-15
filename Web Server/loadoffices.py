752251b576d6dfd8a13aa0a5e970da20d063a7e2
Ekansh Mahendru <ekanshmahendru@gmail.com>
2021-05-15 18:11:46 +0530

    Add department

--- Web Server/loadoffices.py
+++ Web Server/loadoffices.py
@@ -4,7 +4,20 @@ from models import *
 f = open('offices.tsv', 'r')
 
 offices = [x.split('\t') for x in f.read().split('\n')]
-
+departments = [
+                'Computer Science and Engineering', 
+                'Electrical Engineering', 
+                'Mechanical Engineering', 
+                'Chemical Engineering',
+                'Civil Engineering',
+                'Biomedical Engineering',
+                'Humanities and Social Sciences',
+                'Maths Department',
+                'Physics Department',
+                'Chemistry Department',
+                'Metallurgical and Materials Engineering',
+                'Other'
+            ]
 
 with app.app_context():
     OfficeEmails.query.delete()
@@ -13,5 +26,12 @@ with app.app_context():
         obj = OfficeEmails(email, name, 'IITRPR', category)
         db.session.add(obj)
         print("Office: {} added!".format(name))
+
+    db.session.commit()
+
+    for dept in departments:
+        obj = Department(dept)
+        db.session.add(obj)
+        print("Department: {} added!".format(name))
 
     db.session.commit()
